1ea7c962a5c0a4df0bafdfaeb84749a7180b69d8
unknown <merghed.rayen@esprit.tn>
2022-10-17 17:03:56 +0100

    fixx

--- src/main/java/com/esprit/examen/services/ProduitServiceImpl.java
+++ src/main/java/com/esprit/examen/services/ProduitServiceImpl.java
@@ -57,16 +57,16 @@ public class ProduitServiceImpl implements IProduitService {
 		log.info("produit :" + produit);
 		return produit;
 	}
-/*
+
 	@Override
 	public void assignProduitToStock(Long idProduit, Long idStock) {
 		Produit produit = produitRepository.findById(idProduit).orElse(null);
 		Stock stock = stockRepository.findById(idStock).orElse(null);
-		produit.setStock(stock);
+	//	produit.setStock(stock);
 		produitRepository.save(produit);
 
 	}
-	*/
+	
 
 
 }
